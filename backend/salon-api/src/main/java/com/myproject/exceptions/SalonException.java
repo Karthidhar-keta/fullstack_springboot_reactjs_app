b6a9c5d9b69b205ad31e7b4016f99a2bc76d650b
Karthidhar ketha <59505393+Karthidhar-keta@users.noreply.github.com>
2021-05-08 16:03:06 +0530

    Add files via upload

---
+++ backend/salon-api/src/main/java/com/myproject/exceptions/SalonException.java
@@ -0,0 +1,8 @@
+package com.myproject.exceptions;
+
+public class SalonException extends RuntimeException{
+	public SalonException(String messsage) {
+		super(messsage);
+	}
+
+}
---
+++ backend/salon-api/src/main/java/com/myproject/repositories/SalonServiceDetailRepository.java
@@ -0,0 +1,9 @@
+package com.myproject.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.myproject.models.SalonServiceDetail;
+
+public interface SalonServiceDetailRepository extends CrudRepository<SalonServiceDetail, Long> {
+
+}
